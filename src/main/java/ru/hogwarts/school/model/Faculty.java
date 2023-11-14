78c8dfd956ffb2dd1cdb80fbab9b1711023221bf
ibragim955 <amerhanov_ibragim95@mail.ru>
2023-11-14 13:58:52 +0300

    gfh

--- src/main/java/ru/hogwarts/school/model/Faculty.java
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 @Entity
 public class Faculty {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
     private String name;
     private String color;
